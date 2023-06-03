1e35123adf2902862f0768f910ccfa7434c66206
pankajuprety <pankaj.uprety2000@gmail.com>
2023-06-03 16:37:18 +0545

    app accounts, menus & customers created & configured

---
+++ project_restro_menu/app_customers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppCustomersConfig(AppConfig):
+    name = 'app_customers'
---
+++ project_restro_menu/app_menus/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppMenusConfig(AppConfig):
+    name = 'app_menus'
